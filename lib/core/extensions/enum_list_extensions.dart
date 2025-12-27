5ac2475a3f8dcc324ba39029db064950e7940ca2
EliTE709 <logames4freehack@gmail.com>
2025-12-27 07:24:07 +0200

    linter error: ran dart format .

--- lib/core/extensions/enum_list_extensions.dart
+++ lib/core/extensions/enum_list_extensions.dart
@@ -1,7 +1,5 @@
 extension EnumListExtensions<E extends Enum> on List<E> {
   Map<String, E> get mapEnums {
-    return {
-      for (final e in this) e.name: e,
-    };
+    return {for (final e in this) e.name: e};
   }
 }
