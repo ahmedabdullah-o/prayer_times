8a7477a3c372f23d8b66dc6da22d22da4ea7316f
EliTE709 <logames4freehack@gmail.com>
2026-02-02 16:07:53 +0200

    added text to fill negative space

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:prayer_times/core/enums/settings_enums.dart';
+import 'package:prayer_times/core/style/fonts.dart';
 import 'package:prayer_times/features/settings/presentation/widgets/auto_settings.dart';
 import 'package:prayer_times/features/settings/presentation/widgets/settings_item.dart';
 
@@ -25,7 +26,23 @@ class SettingsScreen extends StatelessWidget {
     ];
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      child: Column(spacing: 10, children: [...children]),
+      child: Column(
+        spacing: 10,
+        children: [
+          ...children,
+          // Temporary text to fill negative space
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Prayer Times', style: Fonts.navigationBarItem(true)),
+                Text('GPL v3', style: Fonts.navigationBarItem(false)),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
